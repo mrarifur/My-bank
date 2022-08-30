c3f4b651d575bb164b537d26d3126360c7628e93
mrarifur <arifurrahman735@gmail.com>
2022-08-30 20:16:47 +0300

    Complete-work

--- bank/banking.js
+++ bank/banking.js
@@ -5,7 +5,41 @@ document.getElementById('Deposit-button').addEventListener
     const inputfeild = document.getElementById('Deposit-input');
     const feild = inputfeild.value;
     
-    const depositTotal=document.getElementById('Deposit-total');
-    depositTotal.innerText=feild;
+    const PreviousTotal=document.getElementById('Deposit-total');
+    const currentAmount=PreviousTotal.innerText;
+    const newamount=parseFloat(currentAmount)+parseFloat(feild);
+    PreviousTotal.innerText=newamount;
+
+    const BalancedTotal=document.getElementById('Balace-Total');
+
+    const PreBtotal=BalancedTotal.innerText;
+    const finalTotal= parseFloat(PreBtotal)+parseFloat(feild)
+    BalancedTotal.innerText=finalTotal;
+
+    
+    
     inputfeild.value='';
-})
+});
+
+
+document.getElementById('Withdraw-button').addEventListener('click', function () {
+    const WithdraeInput=document.getElementById('withdraw-input');
+    const Withdrawevalue= parseFloat(WithdraeInput.value);
+    
+    const withdtraweTotal=document.getElementById('Withdrawe-Total');
+    const currenWithdrawe= parseFloat( withdtraweTotal.innerText);
+
+    const TotalWithdraw= Withdrawevalue+currenWithdrawe;
+    withdtraweTotal.innerText=TotalWithdraw;
+    WithdraeInput.value='';
+
+   const BalaceTotal=document.getElementById('Balace-Total');
+   const CurrenBalace= parseFloat(BalaceTotal.innerText);
+   const CurrentTotalBalance= CurrenBalace-Withdrawevalue;
+   BalaceTotal.innerText=CurrentTotalBalance;
+
+    
+} )
+
+
+
